5abc4bd7b82c88f84e4de6626207c8735eab2c85
Jiang <lk11260@gmail.com>
2020-08-10 23:11:28 +0800

    base version

---
+++ README.md
@@ -0,0 +1,4 @@
+<!-- changer START -->
+![](https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjg5NTUyfQ)
+Hello World!^^ 👋
+<!-- changer END -->
---
+++ functions/wjyflh_profile_renderer.js
@@ -0,0 +1,57 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+import gh from 'octonode'
+import axios from "axios";
+import qs from 'qs'
+import Unsplash from "unsplash-js"
+
+
+// Github markdown chnaging section START & END
+const changeStarter = `<!-- changer START -->`
+const changeEnder   = `<!-- changer END -->`
+
+
+// Instantiate Github client from Github API
+const gh_client = gh.client({
+    username: process.env.GH_USERNAME,
+    password: process.env.GH_ACCESS_TOKEN,
+})
+
+// Instantiate Unsplash client from Github API
+// Generated in Unsplash official application dashboard
+const unsplash = new Unsplash({accessKey: process.env.UNSPLASH_ACCESS_TOKEN})
+
+
+
+// Main function here
+const run = async () => {
+    
+    const gh_repo = gh_client.repo(`wjyflh/wjyflh`);
+    let unsplash_search_photos = ``;
+    let searchTopics = ["cloud", "Mountain", "life", "coding"]
+
+    // topics that I wanna search
+    unsplash_search_photos = await unsplash.search.photos(searchTopics[Math.floor(Math.random() * 4)], 1, 10, {orientation: "landscape"})
+        .then(res => (res.json()))
+        .then(data => {
+            return data.results.map(data => data.urls).map(url => url.regular)
+        });
+
+
+    const [readme] = await gh_repo.readmeAsync()
+    const originReadMeContent = Buffer.from(readme.content, 'base64').toString('utf-8')
+    const newContent =
+    `${changeStarter}\n![](${unsplash_search_photos[Math.floor(Math.random() * 5)]})\nHello World!^^ 👋\n${changeEnder}`
+
+    const newReadmeContent = originReadMeContent.split(changeStarter)[0] + newContent + originReadMeContent.split(changeEnder)[1]
+
+    console.log(originReadMeContent)
+    console.log(newReadmeContent)
+
+    await gh_repo.updateContentsAsync(readme.path, 'Update readme contents',newReadmeContent, readme.sha)
+}
+
+
+run()
+
